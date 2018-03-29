c676b31b9e521782f8c7b79971a44e0ecab3b494
jett <jaylong.jett@gmail.com>
2018-03-29 18:10:45 -0500

    adding click game

--- index.php
+++ index.php
@@ -2,11 +2,31 @@
 <html>
 
   <head>
+    <link href='https://fonts.googleapis.com/css?family=Dosis:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
   </head>
 
   <body>
-    testtttttt
+
+     <section id="app">
+            <div id="div1">
+              <button type="submit" @click="updateCount()" id="clickButton">
+              </button>
+            </div>
+
+            <!-- Second status  -->
+            <div id="status"></div>
+            <h1> You clicked <b>{{ numClicks }}</b> times</h1>
+            <h2> You have <b>{{ secs }}</b> secs left </h2>
+
+      </section>
+
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/core.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+      <script src='assets/js/fastclick.js'></script>
+
   </body>
 
 </html>
